06685b3e602f793283e8b2a2a8f85da1b2822f37
DwarakanathU <dwarakanathummadisetti@gmail.com>
2023-01-14 18:14:06 +0530

    Basic Counter functionality is Added

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom/client";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 const NewApp = () => {
+  const [count, setCount] = useState(0); // initializing count varaible with intial value 0 using useState
+
+  // Defining Increment function for counter
+
+  const IncrementByOne = () => {
+    setCount(count + 1);
+  };
+
+  // Defining Increment function for counter
+
+  const DecrementByOne = () => {
+    setCount(count - 1);
+  };
+
   return (
     <>
-      <h2>This is React App</h2>
-      <h1>Hello</h1>;
+      <button onClick={IncrementByOne}>+</button>
+      <span>{count}</span>
+      <button onClick={DecrementByOne}>-</button>
     </>
   );
 };
